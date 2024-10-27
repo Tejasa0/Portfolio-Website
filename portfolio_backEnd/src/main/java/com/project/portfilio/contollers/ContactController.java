f56a00eded165ebe3b5cbc0d8a191f1abd9ff7ae
Tejas Patil <tejaspatila0@gmail.com>
2024-10-27 21:28:39 +0530

    Updating for auto mail for contacts

--- portfolio_backEnd/src/main/java/com/project/portfilio/contollers/ContactController.java
+++ portfolio_backEnd/src/main/java/com/project/portfilio/contollers/ContactController.java
@@ -1,8 +1,11 @@
 package com.project.portfilio.contollers;
 
+import java.io.File;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project.portfilio.model.Contact;
 import com.project.portfilio.repository.ContactRepository;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -24,9 +30,19 @@ public class ContactController {
     public List<Contact> getAllContacts() {
         return contactRepository.findAll();
     }
+    @Autowired
+    private JavaMailSender emailSender;
 
     @PostMapping
-    public Contact createContact(@RequestBody Contact contact) {
+    public Contact createContact(@RequestBody Contact contact) throws MessagingException {
+    	MimeMessage message = emailSender.createMimeMessage();
+	    MimeMessageHelper helper = new MimeMessageHelper(message, true);
+	    helper.setTo(contact.getEmail().toString());
+	    helper.setSubject("Confirmation");
+	    helper.setText("Thank you for contacting me, I will be in touch with you soon");
+//	    helper.setBcc("tejaspatila0@gmail.com");
+//	    helper.addAttachment("Attachment", new File("C:\\Users\\USER\\Downloads\\f1.properties"));
+	    emailSender.send(message);
         return contactRepository.save(contact);
     }
 }
